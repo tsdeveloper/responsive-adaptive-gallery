3108e498947e6b7b4116a6f494c5238d4be358b5
tsdeveloper <ts.analista@gmail.com>
2026-04-23 15:39:55 -0300

    feat: add flex grow, flew shrink e flex basis

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import './App.css'
+import carro1 from './assets/car1.jpg';
+import carro2 from './assets/car2.jpg';
+import carro3 from './assets/car3.jpg';
+import carro4 from './assets/car4.jpg';
+import carro5 from './assets/car5.jpg';
+
 
 function App() {
     return (
         <>
-            <main className="signin-page">
-
+            <main className="gallery__page">
+                <section className="gallery__list">
+                    <figure className="gallery__item">
+                        <img className="gallery__item-image" loading="lazy" src={carro1} alt="car"/>
+                    </figure>
+                    <figure className="gallery__item">
+                        <img className="gallery__item-image" loading="lazy" src={carro2} alt="car"/>
+                    </figure>
+                    <figure className="gallery__item">
+                        <img className="gallery__item-image" loading="lazy" src={carro3} alt="car"/>
+                    </figure>
+                    <figure className="gallery__item">
+                        <img className="gallery__item-image" loading="lazy" src={carro4} alt="car"/>
+                    </figure>
+                    <figure className="gallery__item">
+                        <img className="gallery__item-image" loading="lazy" src={carro5} alt="car"/>
+                    </figure>
+                </section>
             </main>
         </>
     )
